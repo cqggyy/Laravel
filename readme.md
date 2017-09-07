30952fa499b4b15d7bd3621e7a9e243590164a58
cqggyy <cqcar@outlook.com>
2017-09-06 23:59:31 -0400

    sign up page

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 # 设置npm
 
 - 按照教程来的，scripts里面的cross-env全部替换为NODE_ENV，很多人贴出来的提示了，多次对比时都没注意
+
+-自动刷新样式表：npm run watch-poll
--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 
 <h1><a href="{{ route('users.show', $user->id) }}">
-  <img src="{{ $user->gravatar('90') }}" alt="{{ $user->name }}" class="gravatar"/>
+  <img src="{{ $user->gravatar('50') }}" alt="{{ $user->name }}" class="gravatar"/>
 </a>{{ $user->name }}</h1>
