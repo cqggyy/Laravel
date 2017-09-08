e8bec703aa51de696e38f507fdc8fb1e887d8e9c
cqggyy <cqcar@outlook.com>
2017-09-07 23:14:01 -0400

    finish sign up

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,10 +24,18 @@ class UsersController extends Controller
     public function store(Request $request)
     {
       $this->validate($request, [
-        'name'  => 'required|max=50',
+        'name'  => 'required|max:50',
         'email' => 'required|email|unique:users|max:255',
         'password' => 'required'
       ]);
-      return;
+
+      $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+      ]);
+
+      session()->flash('success','欢迎，您将在这里开起一段新的旅程！');
+      return redirect()->route('users.show',[$user]);
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,9 @@
       <h3>注册</h3>
     </div>
     <div class="panel-body">
+
+    @include('shared._errors')
+
       <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
           <div class="form-group">
