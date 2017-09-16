5da706eac80759b76a80c5ba5323892bf3c2bff2
cqggyy <cqcar@outlook.com>
2017-09-16 11:26:46 +0800

    memo modify

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,6 +25,7 @@ class SessionsController extends Controller
         'password' => $request->password,
       ];
 
+      //has remember记住我
       if (Auth::attempt($credentials, $request->has('remember'))){
         session()->flash('success', '欢迎回来！');
         return redirect()->route('users.show', [Auth::user()]);
@@ -32,7 +33,6 @@ class SessionsController extends Controller
         session()->flash('danger', '很抱歉，您的邮箱和密码不匹配！');
         return redirect()->back();
       }
-
 
       return;
     }
